8bb58676bc1d4bc3e5452987f39cdc74fea42714
fujiiree120 <fujiiree120@gmail.com>
2020-01-12 02:08:29 +0900

    レビュー機能の追加#14

---
+++ database/migrations/2020_01_11_145901_create_item_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('item_id');
+            $table->unsignedInteger('item_score');
+            $table->string('item_review_title');
+            $table->string('item_review_comment');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_reviews');
+    }
+}
--- resources/views/items/show_detail.blade.php
+++ resources/views/items/show_detail.blade.php
@@ -9,7 +9,11 @@
            <img src="{{ asset('storage/photos/' . $item->image) }}" class="detail-image">
             <div class="detail-text">
                 <h1 class="border-bottom">{{ $item->name }}</h1>
-                <p class="detail-item-price detail-item-text-margin">価格：{{ $item->price }}円</p>
+                <div class="star-rating">
+                    <div class="star-rating-front" style="width:{{ $item_score }}%">★★★★★</div>
+                    <div class="star-rating-back">★★★★★</div>
+                </div>
+                <p class="detail-item-price">価格：{{ $item->price }}円</p>
                 <ul class="detail-item-text-margin">
                 @forelse($item_comments as $item_comment)
                     <li>{{ $item_comment->item_comments }}</li>
@@ -25,6 +29,44 @@
                  数量：<input  type="text" name="amount">
                 <input type="submit" value="カートに追加" class="btn btn-primary">
             </form>   
-        </div>     
+        </div>  
     </div>
+    <div class="container item-review-field">
+        @forelse($item_reviews as $item_review)
+            <div class="item-review">
+                <div class="star-rating">
+                    <div class="star-rating-front" style="width:{{ $item_review->item_score * 20}}%">★★★★★</div>
+                    <div class="star-rating-back">★★★★★</div>
+                </div>
+                <p  class="border-bottom">{{ $item_review->user->name }}</p>
+                <p class="font-weight-bold">{{ $item_review->item_review_title }}</p>
+                <p>{{ $item_review->item_review_comment }}</p>
+            </div>
+        @empty
+        <p>レビューはまだありません</p>
+        @endforelse
+        <form class=" item-review-field"  method="post" action="{{action('ItemController@create_review') }}">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="review_title">タイトル</label>
+                <input name="title" type="review_title" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="review_body">レビューを記入してください</label>
+                <textarea name="body" rows="3" class="form-control" ></textarea>
+            </div>
+            <div class="form-group">
+                <label for="review_score">点数</label>
+                <select name='score'>
+                     <option value="5">5</option>
+                    <option value="4">4</option>
+                    <option value="3">3</option>
+                    <option value="2">2</option>
+                    <option value="1">1</option>
+                </select>
+            </div>
+            <input type="hidden" name="item_id" value="{{ $item->id }}">
+            <button class="btn btn-primary">レビューを投稿</button>
+        </form>
+    </div> 
 @endsection
